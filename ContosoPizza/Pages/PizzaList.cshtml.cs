8279f60e53e5335827c65f3c61e7f0f5b9045bf4
nicolly.ssousa1 <nicolly.ssousa1@ACL0662727W10-1>
2023-07-27 17:21:40 -0300

    adicionando novos sabores

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -7,6 +7,8 @@ namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
     {
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
         private readonly PizzaService _Service;
         public IList<Pizza> PizzaList { get; set; }= default!;
 
@@ -17,6 +19,23 @@ namespace ContosoPizza.Pages
         public void OnGet()
         {
             PizzaList = _Service.GetPizzas();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _Service.AddPizza(NewPizza);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            _Service.DeletePizza(id);
+            return RedirectToAction("Get");
         }
     }
 }
